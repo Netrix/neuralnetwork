3e8554b6eb4a4d73f29e0697d5eeaa5154e828ec
Kamil Szatkowski <kamil.szatkowski@nsn.com>
2017-01-18 16:53:40 +0100

    Establishing network

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+template<class Type>
+struct Multiplication
+{
+//    Type calculate(
+};
+
+template<class Type>
+struct TrainingEntity
+{
+    std::vector<Type> input;
+    std::vector<Type> output;
+};
+
+
+int main()
+{
+    std::vector<TrainingEntity<double>> TRAIN_DATA = {
+        {{ 1.0, 1.0 }, {0.0}},
+        {{ 1.0, 0.0 }, {1.0}},
+        {{ 0.0, 1.0 }, {1.0}},
+        {{ 0.0, 0.0 }, {0.0}},
+    };
+
+    NetworkBuilder builder;
+    auto yNode = builder.setRootNode(Binary, "add");
+    auto ySubNode = addNode->setFirstInput(BinaryNode, "add");  // n1 + n2 + 2 -> n3 -> y
+    
+    auto n1wMulNode = ySubNode->setFirstInput(BinaryNode, "mul");
+    auto n2wMulNode = ySubNode->setSecondInput(BinaryNode, "mul");
+    yNode->setSecondInput(Variable); // w30
+
+    n1wMulNode->setFirstInput(Variable); // w31
+    n2wMulNode->setFirstInput(Variable); // w32
+
+    auto n1Node = n1wMulNode->setSecondInput(BinaryNode, "sum");
+    auto n2Node = n2wMulNode->setSecondInput(BinaryNode, "sum");
+
+    auto n1SubNode = n1Node->setFirstInput(BinaryNode, "sum");
+    auto n2SubNode = n2Node->setFirstInput(BinaryNode, "sum");
+
+    n1Node->setSecondInput(Variable); // w10
+    n2Node->setSecondInput(Variable); // w20
+
+    auto x1w11MulNode = n1SubNode->setFirstInput(BinaryNode, "mul");
+    auto x2w12MulNode = n1SubNode->setSecondInput(BinaryNode, "mul");
+
+    auto x1w21MulNode = n2SubNode->setFirstInput(BinaryNode, "mul");
+    auto x2w22MulNode = n2SubNode->setSecondInput(BinaryNode, "mul");
+
+    auto x1Node = x1w11MulNode->setFirstInput(Const);
+    x1w11MulNode->setSecondInput(Variable); // w11
+
+    auto x2Node = x2w12MulNode->setFirstInput(Const);
+    x2w12MulNode->setSecondInput(Variable); // w12
+
+    x1w21MulNode->setFirstInput(x1Node);
+    x1w21MulNode->setSecondInput(Variable); // w21
+
+    x2w22MulNode->setFirstInput(x2Node);
+    x2w22MulNode->setSecondInput(Variable); // w22
+
+    auto network = builder.build();
+
+    std::cout << "Hello" << std::endl;
+}
