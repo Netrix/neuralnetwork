69c717644a284219c5163dee53c32f3f7fc33bb8
Kamil Szatkowski <kamil.szatkowski@gmail.com>
2017-01-24 08:35:04 +0100

    Added MnistPrepocessedDataSet

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,56 @@
 #include <set>
 #include "NormalDistributionGenerator.hpp"
 
+template<class Type>
+struct MnistPreprocessedDataSet
+{
+    MnistPreprocessedDataSet(MnistDataset const& mnistData)
+        : m_inputSampleSize(mnistData.imageWidth * mnistData.imageHeight)
+        , m_inputData(mnistData.imageData.size())
+        , m_outputData(m_outputSampleSize * mnistData.labels.size())
+    {
+        std::transform(std::begin(mnistData.imageData), std::end(mnistData.imageData), std::begin(m_inputData),
+                       [](auto v)
+        {
+            return (static_cast<Type>(v) / 255.0) - 0.5;
+        });
+
+        for(std::size_t i = 0, v = 0; i < m_outputData.size(); ++v)
+        {
+            for(std::size_t k = 0; k < 10; ++k, ++i)
+            {
+                m_outputData[i] = mnistData.labels[v] == k ? 1.0 : 0.0;
+            }
+        }
+    }
+
+    auto getInputSampleSize() const
+    {
+        return m_inputSampleSize;
+    }
+
+    auto getOutputSampleSize() const
+    {
+        return m_outputSampleSize;
+    }
+
+    auto getInputSample(std::size_t sampleIndex) const
+    {
+        return ArrayView<Type const>(m_inputData.data() + m_inputSampleSize * sampleIndex, m_inputSampleSize);
+    }
+
+    auto getOutputSample(std::size_t sampleIndex) const
+    {
+        return ArrayView<Type const>(m_outputData.data() + m_outputSampleSize * sampleIndex, m_outputSampleSize);
+    }
+
+private:
+    std::size_t m_inputSampleSize;
+    std::vector<Type> m_inputData;
+
+    std::size_t m_outputSampleSize = 10;
+    std::vector<Type> m_outputData;
+};
 
 int main()
 {
@@ -44,6 +94,8 @@ int main()
     network->setLearningRate(0.01f);
 
     network->setVariables(NormalDistributionGenerator<BNN_TYPE>(17, 0, 1e-1));
+
+//    void learnNetwork(ArrayView<BNN_TYPE> inputData)
 
 
     //TODO to ponizej ubrac w klase
