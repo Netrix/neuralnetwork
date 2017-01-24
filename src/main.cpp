4b4c7b0e6c375b5684938145c264bcafeb2ee24d
Kamil Szatkowski <kamil.szatkowski@nsn.com>
2017-01-24 14:35:20 +0100

    Learning MNIST

--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,13 @@ template<class Type>
 struct MnistPreprocessedDataSet
 {
     MnistPreprocessedDataSet(MnistDataset const& mnistData)
-        : m_inputSampleSize(mnistData.imageWidth * mnistData.imageHeight)
+        : m_numSamples(mnistData.labels.size())
+        , m_inputSampleSize(mnistData.imageWidth * mnistData.imageHeight)
         , m_inputData(mnistData.imageData.size())
         , m_outputData(m_outputSampleSize * mnistData.labels.size())
     {
+        assert(mnistData.labels.size() == (mnistData.imageData.size() / m_inputSampleSize));
+
         std::transform(std::begin(mnistData.imageData), std::end(mnistData.imageData), std::begin(m_inputData),
                        [](auto v)
         {
@@ -63,7 +66,14 @@ struct MnistPreprocessedDataSet
         return ArrayView<Type const>(m_outputData.data() + m_outputSampleSize * sampleIndex, m_outputSampleSize);
     }
 
+    auto getNumSamples() const
+    {
+        return m_numSamples;
+    }
+
 private:
+    std::size_t m_numSamples;
+
     std::size_t m_inputSampleSize;
     std::vector<Type> m_inputData;
 
@@ -71,32 +81,54 @@ private:
     std::vector<Type> m_outputData;
 };
 
-int main()
-{
-    std::string imagesPath = "../../../data/train-images-idx3-ubyte";
-    std::string labelsPath = "../../../data/train-labels-idx1-ubyte";
-
-    auto mnistDataset = loadMnistDataset(imagesPath, labelsPath);
-    std::cout << mnistDataset.imageWidth << " " << mnistDataset.imageHeight << " " << mnistDataset.imageData.size() << " " << mnistDataset.labels.size() << " " << (int)mnistDataset.labels[0] << std::endl;
-
-    std::set<int> labelsSet(std::begin(mnistDataset.labels), std::end(mnistDataset.labels));
-
-    for(auto s : labelsSet)
+    template<class Dataset, class Network>
+    auto learnEpoch(Network & network, Dataset const& dataset, std::size_t batchSize)
     {
-        std::cout << s << " ";
+        BNN_TYPE errorSum = 0;
+        for(std::size_t i = 0; i < dataset.getNumSamples();)
+        {
+            for(std::size_t k = 0; k < batchSize && i < dataset.getNumSamples(); ++i, ++k)
+            {
+                auto result = make_math_adapter(network->forwardPass(dataset.getInputSample(i)));
+//                std::cout << std::fixed << std::setprecision(2) << "Input: " << trainEntity.input << ", expected result: " <<trainEntity.output << ", result: " << result << std::endl;
+                auto expected = make_math_adapter(dataset.getOutputSample(i));
+                auto error = expected - result;
+                network->backPropagate(error);
+
+                auto squaredError = error * error;
+                errorSum += squaredError.sum();
+            }
+            network->applyDeltaOnVariables();
+            std::cout << "batch completed, samples passed: " << i << "/" << dataset.getNumSamples() << std::endl;
+        }
+        return errorSum;
     }
 
+
+int main()
+{
+    std::string imagesPath = "../../data/train-images-idx3-ubyte";
+    std::string labelsPath = "../../data/train-labels-idx1-ubyte";
+
+    auto mnistDataset = MnistPreprocessedDataSet<float>(loadMnistDataset(imagesPath, labelsPath));
+    std::cout << mnistDataset.getInputSampleSize() << " " << mnistDataset.getNumSamples() << " " << mnistDataset.getOutputSampleSize() << std::endl;
+
     LayeredNetworkBuilder LayeredNetworkBuilder;
-    auto outLayer = LayeredNetworkBuilder.setOutputLayer(FullyConnectedLayerSpecs{10, "tanh"});
-    auto hiddenLayer = outLayer->setInputLayer(FullyConnectedLayerSpecs{32, "tanh"});
-    hiddenLayer->setInputLayer(InputLayerSpecs{mnistDataset.imageHeight * mnistDataset.imageWidth});
+    auto outLayer = LayeredNetworkBuilder.setOutputLayer(FullyConnectedLayerSpecs{mnistDataset.getOutputSampleSize(), "relu"});
+    auto hiddenLayer = outLayer->setInputLayer(FullyConnectedLayerSpecs{32, "relu"});
+    hiddenLayer->setInputLayer(InputLayerSpecs{mnistDataset.getInputSampleSize()});
     auto network = LayeredNetworkBuilder.buildBackPropagationNetwork();
     network->setLearningRate(0.01f);
 
     network->setVariables(NormalDistributionGenerator<BNN_TYPE>(17, 0, 1e-1));
 
-//    void learnNetwork(ArrayView<BNN_TYPE> inputData)
 
+    for(int i = 0; i < 500; ++i)
+    {
+        auto errorSum = learnEpoch(network, mnistDataset, 256);
+        std::cout << "epoch: " << i << " errorSum: " << errorSum <<  std::endl;
+
+    }
 
     //TODO to ponizej ubrac w klase
 //    for(int i = 0; i < 500; ++i)
